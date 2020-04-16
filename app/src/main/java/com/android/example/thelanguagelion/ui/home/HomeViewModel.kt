aa526a0a9d6de08e62d6534a59d294bd31fe62ba
Rayo Verweij <rayo.verweij@live.nl>
2020-04-16 16:44:08 -0400

    Random welcome messages

--- app/src/main/java/com/android/example/thelanguagelion/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/android/example/thelanguagelion/ui/home/HomeViewModel.kt
@@ -6,12 +6,17 @@ import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
 
+    private val welcomeMessages = listOf(
+        "Welcome back, Rayo!\nKeep up the good work :)",
+        "A lesson a day\nkeeps the boredom away!",
+        "Your Dutch lessons\nwon't take themselves!"
+    )
+
     private val _text = MutableLiveData<String>()
     val text: LiveData<String>
         get() = _text
 
-
     init {
-        _text.value = "Welcome back, Rayo!\nKeep up the good work :)"
+        _text.value = welcomeMessages.random()
     }
 }
